a1c0d29f80d3459051b87b8a879f2287bac6afd6
Sulav Basnet <mysulhub@gmail.com>
2023-12-15 20:54:49 +0545

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:whattodo/screens/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'WhatTodo',
+      home: Home(),
+    );
+  }
+}
